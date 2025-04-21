25c708db2ca2475e00848544e06d465fd53d4fd9
Aarchish Jindal <aarchish0@gmail.com>
2025-04-21 12:51:34 +0530

    round 2

--- Round-2/assignment.py
+++ Round-2/assignment.py
@@ -1,9 +1,11 @@
 import random
 
+## User input for grid size ##
 print("Enter the grid size n:")
 n = int(input())
 last_grid_value = n*n
 
+## Define computational variables and arrays ##
 player1_pos_current = 0
 player2_pos_current = 0
 player3_pos_current = 0
@@ -16,7 +18,28 @@ player1_dice_history = []
 player2_dice_history = []
 player3_dice_history = []
 
+player1_coordinate_history = []
+player2_coordinate_history = []
+player3_coordinate_history = []
 
+coordinate_table = []
+
+## Generate Co-ordinate table based on grid size ##
+for i in range(1,last_grid_value+1):
+    index = i-1
+    row = index//n
+    if(row%2 == 0):
+        col = index%n
+        coordinate = [col, row]
+        coordinate_table.append(coordinate)
+    else:
+        col = n - index%n - 1
+        coordinate = [col, row]
+        coordinate_table.append(coordinate)
+
+print("Coordinate Table: ", coordinate_table)
+
+## Generate Random movement of 3 players on a user-defined grid size ##
 while(player1_pos_current != last_grid_value or
       player2_pos_current != last_grid_value or
       player3_pos_current != last_grid_value):
@@ -57,16 +80,37 @@ while(player1_pos_current != last_grid_value or
         player2_pos_history.append(player2_pos_current)
     if(player2_pos_current == last_grid_value): break
 
+## Create the player coordinate history based on their actual movements in the game ##
+for i in player1_pos_history:
+    if(i == 0):
+        player1_coordinate_history.append([-1,-1])
+    else:
+        player1_coordinate_history.append(coordinate_table.__getitem__(i-1))
 
+for i in player2_pos_history:
+    if(i == 0):
+        player2_coordinate_history.append([-1,-1])
+    else:
+        player2_coordinate_history.append(coordinate_table.__getitem__(i-1))
+
+for i in player3_pos_history:
+    if(i == 0):
+        player3_coordinate_history.append([-1,-1])
+    else:
+        player3_coordinate_history.append(coordinate_table.__getitem__(i-1))
+
+## Generate outputs of the game ##
 print("Player No: 1")
 print("Dice Roll History: ", player1_dice_history)
 print("Position History: ", player1_pos_history)
+print("Coordinate History: ", player1_coordinate_history)
 if(player1_pos_current == last_grid_value):
     print("Winner Status: Yes")
 
 print("Player No: 2")
 print("Dice Roll History: ", player2_dice_history)
 print("Position History: ", player2_pos_history)
+print("Coordinate History: ", player2_coordinate_history)
 if(player2_pos_current == last_grid_value):
     print("Winner Status: Yes")
 
@@ -74,5 +118,7 @@ if(player2_pos_current == last_grid_value):
 print("Player No: 3")
 print("Dice Roll History: ", player3_dice_history)
 print("Position History: ", player3_pos_history)
+print("Coordinate History: ", player3_coordinate_history)
 if(player3_pos_current == last_grid_value):
     print("Winner Status: Yes")
+
